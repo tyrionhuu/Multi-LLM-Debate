bf7c2ae0c5c84b17b943b624f175d9980b138025
Tyrion Huu <tyrionhuu@gmail.com>
2025-02-17 18:56:08 +0800

    Refactor prompt formatting to use JSON structure for responses

--- multi_llm_debate/llm/prompts.py
+++ multi_llm_debate/llm/prompts.py
@@ -1,20 +1,31 @@
 from typing import List
 
 NEW_LINE = "\n"
-
+JSON_FORMAT = """
+{
+    "reasoning": "your reasoning based on the passage",
+    "answer": "Yes/No"
+}
+"""
+JSON_FORMAT_COT = """
+{
+    "reasoning": {
+        "step_1": "first step of your reasoning",
+        "step_2": "second step of your reasoning",
+        "step_3": "third step of your reasoning",
+        "...": "continue with as many steps as needed"
+    },
+    "answer": "Yes/No"
+}
+"""
 
 def build_bool_q_round_zero_prompt(
     question: str, passage: str, use_cot: bool = True
 ) -> str:
     prompt = "You will be given a yes-no question which is based on a passage. "
-    prompt += "Answer in the following format:" + NEW_LINE
-    if use_cot:
-        prompt += (
-            "Reasoning: [Your step-by-step reasoning based on the passage]" + NEW_LINE
-        )
-    else:
-        prompt += "Reasoning: [Your reasoning based on the passage]" + NEW_LINE
-    prompt += "Final Answer: [Yes/No]" + NEW_LINE + NEW_LINE
+    prompt += "Answer in the following JSON format:" + NEW_LINE
+    prompt += JSON_FORMAT_COT if use_cot else JSON_FORMAT
+    prompt += NEW_LINE
     prompt += "Question: " + question + NEW_LINE
     prompt += "Passage: " + passage
 
@@ -37,18 +48,9 @@ def build_bool_q_round_n_prompt(
         "Consider these responses when answering the following yes-no question."
         + NEW_LINE
     )
-    prompt += "Answer in the following format:" + NEW_LINE
-    if use_cot:
-        prompt += (
-            "Reasoning: [Your step-by-step reasoning based on the passage and other responses]"
-            + NEW_LINE
-        )
-    else:
-        prompt += (
-            "Reasoning: [Your reasoning based on the passage and other responses]"
-            + NEW_LINE
-        )
-    prompt += "Final Answer: [Yes/No]" + NEW_LINE + NEW_LINE
+    prompt += "Answer in the following JSON format:" + NEW_LINE
+    prompt += JSON_FORMAT_COT if use_cot else JSON_FORMAT
+    prompt += NEW_LINE
     prompt += "Question: " + question + NEW_LINE
     prompt += "Passage: " + passage
 
