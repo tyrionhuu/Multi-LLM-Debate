171bb39b833e3c91062006df610fb78d4a34bf59
Tyrion Huu <tyrionhuu@gmail.com>
2025-02-17 18:45:56 +0800

    Move save_api_key import from shared to utils directory

--- multi_llm_debate/llm/llm.py
+++ multi_llm_debate/llm/llm.py
@@ -18,7 +18,7 @@ BASE_URL = get_base_url()
 
 if KEY.strip() == "":
     KEY = input("Please enter your API key: ")
-    from ..shared.config_manager import save_api_key
+    from ..utils.config_manager import save_api_key
 
     save_api_key(KEY)
 
