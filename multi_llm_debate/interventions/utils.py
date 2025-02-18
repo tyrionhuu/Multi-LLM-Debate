084786fc218eba1a6d3475bff0bd8b154f4341f8
Tyrion Huu <tyrionhuu@gmail.com>
2025-02-18 10:19:47 +0800

    Add docstring to compute_sentence_embedding function in utils.py

--- multi_llm_debate/interventions/utils.py
+++ multi_llm_debate/interventions/utils.py
@@ -3,4 +3,13 @@ from sentence_transformers import SentenceTransformer
 from scipy.spatial.distance import cosine
 
 def compute_sentence_embedding(model: SentenceTransformer, sentence: str) -> np.ndarray:
+    """Compute the embedding vector for a given sentence using a SentenceTransformer model.
+
+    Args:
+        model: A SentenceTransformer model instance used for encoding.
+        sentence: The input text to be encoded into an embedding vector.
+
+    Returns:
+        A numpy array containing the sentence embedding vector.
+    """
     return model.encode([sentence])[0]
