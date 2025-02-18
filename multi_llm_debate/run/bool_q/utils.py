ec0f4fe75d5122f130b2deea495a5e3c4c950c42
Tyrion Huu <tyrionhuu@gmail.com>
2025-02-18 13:04:25 +0800

    Simplify ID generation in process_bool_q_df function by using index + 1 instead of hash

--- multi_llm_debate/run/bool_q/utils.py
+++ multi_llm_debate/run/bool_q/utils.py
@@ -1,8 +1,4 @@
-
 import pandas as pd
-
-from ...utils.generate_hash import generate_hash
-
 
 
 def process_bool_q_df(dataframe: pd.DataFrame) -> pd.DataFrame:
@@ -19,8 +15,6 @@ def process_bool_q_df(dataframe: pd.DataFrame) -> pd.DataFrame:
     
     # Generate ID from index if 'id' column doesn't exist
     if 'id' not in processed_df.columns:
-        processed_df['id'] = processed_df.index.map(
-            lambda x: f"boolq_{generate_hash(x)}"
-        )
+        processed_df['id'] = processed_df.index + 1
 
     return processed_df
