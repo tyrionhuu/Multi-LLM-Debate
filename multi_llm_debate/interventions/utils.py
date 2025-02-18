a495f4856bb0ac09d4f63da25391751373113893
Tyrion Huu <tyrionhuu@gmail.com>
2025-02-18 10:19:16 +0800

    Add sentence-transformers dependency and implement compute_sentence_embedding function in utils.py

--- multi_llm_debate/interventions/utils.py
+++ multi_llm_debate/interventions/utils.py
@@ -1,3 +1,6 @@
 import numpy as np
 from sentence_transformers import SentenceTransformer
 from scipy.spatial.distance import cosine
+
+def compute_sentence_embedding(model: SentenceTransformer, sentence: str) -> np.ndarray:
+    return model.encode([sentence])[0]
