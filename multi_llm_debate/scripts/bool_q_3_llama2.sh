3b53badca5f8052b2d38fef6e691a874cf9bbddf
Tianyu Hu <tyrionhuu@gmail.com>
2025-02-20 12:13:34 +0800

    add script

---
+++ multi_llm_debate/scripts/bool_q_3_llama2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run the evaluation using module path
+python -m multi_llm_debate.run.bool_q.main \
+    --config ./multi_llm_debate/configs/3_llama2.json \
+    --sample-size 2000 \
+    --max-workers 4
