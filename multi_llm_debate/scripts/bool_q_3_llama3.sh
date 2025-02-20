894b7e82b408ea9f7989fce57058b1ab1199740d
Tianyu Hu <tyrionhuu@gmail.com>
2025-02-20 12:11:36 +0800

    modify

--- multi_llm_debate/scripts/bool_q_3_llama3.sh
+++ multi_llm_debate/scripts/bool_q_3_llama3.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Run from the script directory
-cd "$(dirname "$0")"
-
 # Run the evaluation using module path
 python -m multi_llm_debate.run.bool_q.main \
-    --config multi_llm_debate/configs/3_llama3.json \
+    --config ./multi_llm_debate/configs/3_llama3.json \
     --sample-size 2000 \
     --max-workers 4
