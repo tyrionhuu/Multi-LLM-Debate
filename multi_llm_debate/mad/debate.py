bfab877f93416a879f24ac1ed3e8c3b091079b6d
Tyrion Huu <tyrionhuu@gmail.com>
2025-03-25 10:38:54 +0800

    refactor: Improve code readability by formatting and organizing methods
    - Reformat method signatures and body for consistency
    - Enhance readability by aligning parameters and arguments
    - Update comments for clarity

--- multi_llm_debate/mad/debate.py
+++ multi_llm_debate/mad/debate.py
@@ -1,22 +1,24 @@
-import os
 import json
+import os
 import random
+
 random.seed(0)
 from .agent import Agent
 
-NAME_LIST=[
+NAME_LIST = [
     "Affirmative side",
     "Negative side",
     "Moderator",
 ]
 
+
 class DebatePlayer(Agent):
     def __init__(
-        self, 
-        model_name: str, 
-        name: str, 
-        temperature:float, 
-        provider: str="ollama",
+        self,
+        model_name: str,
+        name: str,
+        temperature: float,
+        provider: str = "ollama",
         sleep_time: float = 0,
     ) -> None:
         """Create a player in the debate
@@ -28,19 +30,22 @@ class DebatePlayer(Agent):
             openai_api_key (str): As the parameter name suggests
             sleep_time (float): sleep because of rate limits
         """
-        super(DebatePlayer, self).__init__(model_name, name, temperature, provider, sleep_time)
+        super(DebatePlayer, self).__init__(
+            model_name, name, temperature, provider, sleep_time
+        )
 
 
 class Debate:
-    def __init__(self,
-            model_name: str='gpt-3.5-turbo', 
-            temperature: float=0, 
-            num_players: int=3,
-            provider: str="ollama",
-            config: dict=None,
-            max_round: int=3,
-            sleep_time: float=0
-        ) -> None:
+    def __init__(
+        self,
+        model_name: str = "gpt-3.5-turbo",
+        temperature: float = 0,
+        num_players: int = 3,
+        provider: str = "ollama",
+        config: dict = None,
+        max_round: int = 3,
+        sleep_time: float = 0,
+    ) -> None:
         """Create a debate
 
         Args:
@@ -67,10 +72,12 @@ class Debate:
         self.creat_agents()
         self.init_agents()
 
-
     def init_prompt(self):
         def prompt_replace(key):
-            self.config[key] = self.config[key].replace("##debate_topic##", self.config["debate_topic"])
+            self.config[key] = self.config[key].replace(
+                "##debate_topic##", self.config["debate_topic"]
+            )
+
         prompt_replace("player_meta_prompt")
         prompt_replace("moderator_meta_prompt")
         prompt_replace("affirmative_prompt")
@@ -79,7 +86,14 @@ class Debate:
     def creat_agents(self):
         # creates players
         self.players = [
-            DebatePlayer(model_name=self.model_name, name=name, temperature=self.temperature, provider=self.provider, sleep_time=self.sleep_time) for name in NAME_LIST
+            DebatePlayer(
+                model_name=self.model_name,
+                name=name,
+                temperature=self.temperature,
+                provider=self.provider,
+                sleep_time=self.sleep_time,
+            )
+            for name in NAME_LIST
         ]
         self.affirmative = self.players[0]
         self.negative = self.players[1]
@@ -87,29 +101,45 @@ class Debate:
 
     def init_agents(self):
         # start: set meta prompt
-        self.affirmative.set_meta_prompt(self.config['player_meta_prompt'])
-        self.negative.set_meta_prompt(self.config['player_meta_prompt'])
-        self.moderator.set_meta_prompt(self.config['moderator_meta_prompt'])
-        
+        self.affirmative.set_meta_prompt(self.config["player_meta_prompt"])
+        self.negative.set_meta_prompt(self.config["player_meta_prompt"])
+        self.moderator.set_meta_prompt(self.config["moderator_meta_prompt"])
+
         # start: first round debate, state opinions
         print(f"===== Debate Round-1 =====\n")
-        self.affirmative.add_event(self.config['affirmative_prompt'])
+        self.affirmative.add_event(self.config["affirmative_prompt"])
         self.aff_ans = self.affirmative.ask()
         self.affirmative.add_memory(self.aff_ans)
-        self.config['base_answer'] = self.aff_ans
+        self.config["base_answer"] = self.aff_ans
 
-        self.negative.add_event(self.config['negative_prompt'].replace('##aff_ans##', self.aff_ans))
+        self.negative.add_event(
+            self.config["negative_prompt"].replace("##aff_ans##", self.aff_ans)
+        )
         self.neg_ans = self.negative.ask()
         self.negative.add_memory(self.neg_ans)
 
-        self.moderator.add_event(self.config['moderator_prompt'].replace('##aff_ans##', self.aff_ans).replace('##neg_ans##', self.neg_ans).replace('##round##', 'first'))
+        self.moderator.add_event(
+            self.config["moderator_prompt"]
+            .replace("##aff_ans##", self.aff_ans)
+            .replace("##neg_ans##", self.neg_ans)
+            .replace("##round##", "first")
+        )
         self.mod_ans = self.moderator.ask()
         self.moderator.add_memory(self.mod_ans)
         self.mod_ans = eval(self.mod_ans)
 
     def round_dct(self, num: int):
         dct = {
-            1: 'first', 2: 'second', 3: 'third', 4: 'fourth', 5: 'fifth', 6: 'sixth', 7: 'seventh', 8: 'eighth', 9: 'ninth', 10: 'tenth'
+            1: "first",
+            2: "second",
+            3: "third",
+            4: "fourth",
+            5: "fifth",
+            6: "sixth",
+            7: "seventh",
+            8: "eighth",
+            9: "ninth",
+            10: "tenth",
         }
         return dct[num]
 
@@ -125,7 +155,7 @@ class Debate:
         print(self.config["Reason"])
 
     def broadcast(self, msg: str):
-        """Broadcast a message to all players. 
+        """Broadcast a message to all players.
         Typical use is for the host to announce public information
 
         Args:
@@ -136,7 +166,7 @@ class Debate:
             player.add_event(msg)
 
     def speak(self, speaker: str, msg: str):
-        """The speaker broadcast a message to all other players. 
+        """The speaker broadcast a message to all other players.
 
         Args:
             speaker (str): name of the speaker
@@ -154,53 +184,71 @@ class Debate:
         player.add_memory(ans)
         self.speak(player.name, ans)
 
-
     def run(self):
 
         for round in range(self.max_round - 1):
 
-            if self.mod_ans["debate_answer"] != '':
+            if self.mod_ans["debate_answer"] != "":
                 break
             else:
                 print(f"===== Debate Round-{round+2} =====\n")
-                self.affirmative.add_event(self.config['debate_prompt'].replace('##oppo_ans##', self.neg_ans))
+                self.affirmative.add_event(
+                    self.config["debate_prompt"].replace("##oppo_ans##", self.neg_ans)
+                )
                 self.aff_ans = self.affirmative.ask()
                 self.affirmative.add_memory(self.aff_ans)
 
-                self.negative.add_event(self.config['debate_prompt'].replace('##oppo_ans##', self.aff_ans))
+                self.negative.add_event(
+                    self.config["debate_prompt"].replace("##oppo_ans##", self.aff_ans)
+                )
                 self.neg_ans = self.negative.ask()
                 self.negative.add_memory(self.neg_ans)
 
-                self.moderator.add_event(self.config['moderator_prompt'].replace('##aff_ans##', self.aff_ans).replace('##neg_ans##', self.neg_ans).replace('##round##', self.round_dct(round+2)))
+                self.moderator.add_event(
+                    self.config["moderator_prompt"]
+                    .replace("##aff_ans##", self.aff_ans)
+                    .replace("##neg_ans##", self.neg_ans)
+                    .replace("##round##", self.round_dct(round + 2))
+                )
                 self.mod_ans = self.moderator.ask()
                 self.moderator.add_memory(self.mod_ans)
                 self.mod_ans = eval(self.mod_ans)
 
-        if self.mod_ans["debate_answer"] != '':
+        if self.mod_ans["debate_answer"] != "":
             self.config.update(self.mod_ans)
-            self.config['success'] = True
+            self.config["success"] = True
 
         # ultimate deadly technique.
         else:
-            judge_player = DebatePlayer(model_name=self.model_name, name='Judge', temperature=self.temperature, provider=self.provider, sleep_time=self.sleep_time)
-            aff_ans = self.affirmative.memory_lst[2]['content']
-            neg_ans = self.negative.memory_lst[2]['content']
+            judge_player = DebatePlayer(
+                model_name=self.model_name,
+                name="Judge",
+                temperature=self.temperature,
+                provider=self.provider,
+                sleep_time=self.sleep_time,
+            )
+            aff_ans = self.affirmative.memory_lst[2]["content"]
+            neg_ans = self.negative.memory_lst[2]["content"]
 
-            judge_player.set_meta_prompt(self.config['moderator_meta_prompt'])
+            judge_player.set_meta_prompt(self.config["moderator_meta_prompt"])
 
             # extract answer candidates
-            judge_player.add_event(self.config['judge_prompt_last1'].replace('##aff_ans##', aff_ans).replace('##neg_ans##', neg_ans))
+            judge_player.add_event(
+                self.config["judge_prompt_last1"]
+                .replace("##aff_ans##", aff_ans)
+                .replace("##neg_ans##", neg_ans)
+            )
             ans = judge_player.ask()
             judge_player.add_memory(ans)
 
             # select one from the candidates
-            judge_player.add_event(self.config['judge_prompt_last2'])
+            judge_player.add_event(self.config["judge_prompt_last2"])
             ans = judge_player.ask()
             judge_player.add_memory(ans)
-            
+
             ans = eval(ans)
-            if ans["debate_answer"] != '':
-                self.config['success'] = True
+            if ans["debate_answer"] != "":
+                self.config["success"] = True
                 # save file
             self.config.update(ans)
             self.players.append(judge_player)
@@ -217,9 +265,11 @@ if __name__ == "__main__":
         debate_topic = ""
         while debate_topic == "":
             debate_topic = input(f"\nEnter your debate topic: ")
-            
-        config = json.load(open(f"{MAD_path}/code/utils/config4all.json", "r"))
-        config['debate_topic'] = debate_topic
 
-        debate = Debate(num_players=3, provider="ollama", config=config, temperature=0, sleep_time=0)
+        config = json.load(open(f"{MAD_path}/code/utils/config4all.json", "r"))
+        config["debate_topic"] = debate_topic
+
+        debate = Debate(
+            num_players=3, provider="ollama", config=config, temperature=0, sleep_time=0
+        )
         debate.run()
