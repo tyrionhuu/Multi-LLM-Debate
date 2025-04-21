feee8cff11249eb3e1055f3b10b9597af4c9146b
Tyrion Huu <tyrionhuu@gmail.com>
2025-04-21 12:55:51 +0800

    chore: Remove obsolete BoolQ evaluation scripts
    Delete outdated shell scripts for BoolQ evaluations across various models.

--- multi_llm_debate/scripts/bool_q/bool_q_llama2_2_llama3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run the evaluation using module path
-python -m multi_llm_debate.run.bool_q.main \
-    --config ./multi_llm_debate/configs/2_llama2_llama3.json \
-    --sample-size 2000 \
-    --max-workers 4
