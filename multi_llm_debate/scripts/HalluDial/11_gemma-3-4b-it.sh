c80aacd92a2ea1f43ac810562cb1fe18f523079b
Tianyu Hu <tyrionhuu@gmail.com>
2025-04-22 18:28:13 +0800

    feat: Update evaluation script to include pruning options
    Add support for quality and diversity pruning in the HalluDial evaluation script, enhancing model performance tuning capabilities.

--- multi_llm_debate/scripts/HalluDial/11_gemma-3-4b-it.sh
+++ multi_llm_debate/scripts/HalluDial/11_gemma-3-4b-it.sh
@@ -108,13 +108,12 @@ CONFIG='[
 CUDA_VISIBLE_DEVICES=all python -m multi_llm_debate.run.hallu_dial.main \
     --config-json "$CONFIG" \
     --task-name "hallu_dial" \
-    --sample-size 2 \
 # Run the evaluation using module path with direct JSON config
-# CUDA_VISIBLE_DEVICES=all python -m multi_llm_debate.run.hallu_dial.main \
-#     --config-json "$CONFIG" \
-#     --task-name "hallu_dial_pruning" \
-#     --quality-pruning \
-#     --diversity-pruning "embedding" \
-#     --pruning-amount 5 \
+CUDA_VISIBLE_DEVICES=all python -m multi_llm_debate.run.hallu_dial.main \
+    --config-json "$CONFIG" \
+    --task-name "hallu_dial_pruning" \
+    --quality-pruning \
+    --diversity-pruning "embedding" \
+    --pruning-amount 5 \
 
 cleanup 1
