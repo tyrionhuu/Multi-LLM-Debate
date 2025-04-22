5ea3b2f567235e87cfe3cf2dad1f7184907ce438
Tianyu Hu <tyrionhuu@gmail.com>
2025-04-22 11:05:27 +0800

    fix: Update port assignment for GPU script to avoid conflicts
    Changed the port assignment calculation to start from 8600 instead of 8000.

--- multi_llm_debate/scripts/BIG-Bench/11_gemma-3-4b-it.sh
+++ multi_llm_debate/scripts/BIG-Bench/11_gemma-3-4b-it.sh
@@ -54,7 +54,7 @@ trap cleanup SIGINT SIGTERM EXIT
 
 # For port, use the first GPU in case of multiple GPUs
 FIRST_GPU=$(echo $GPU | cut -d',' -f1)
-PORT=$((8000 + FIRST_GPU * 10))
+PORT=$((8600 + FIRST_GPU * 10))
 
 # export VLLM_LOGGING_LEVEL=ERROR
 
