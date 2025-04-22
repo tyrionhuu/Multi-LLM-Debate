33b7ef0fa04179f5376db6efcadbcfd7f479a8e7
Tianyu Hu <tyrionhuu@gmail.com>
2025-04-22 19:07:08 +0800

    modify

--- multi_llm_debate/scripts/ICE-Score/11_gemma-3-4b-it.sh
+++ multi_llm_debate/scripts/ICE-Score/11_gemma-3-4b-it.sh
@@ -106,7 +106,9 @@ CONFIG='[
 
 # Run the evaluation using module path with direct JSON config
 CUDA_VISIBLE_DEVICES=all python -m multi_llm_debate.run.ice_score.main \
-    --config-json "$CONFIG"
+    --config-json "$CONFIG" \
+    --task-name "ice_score" \
+    
 # Run the evaluation using module path with direct JSON config
 CUDA_VISIBLE_DEVICES=all python -m multi_llm_debate.run.ice_score.main \
     --config-json "$CONFIG" \
