d9cc21afb94d18ef9270c60e3c64ca7e76a2a961
Tyrion Huu <tyrionhuu@gmail.com>
2025-04-22 10:41:16 +0800

    feat: Implement main script for BIG-Bench evaluation
    Add functionality to run evaluations for the BIG-Bench dataset, including
    argument parsing and dataset loading.

--- multi_llm_debate/run/big_bench/main.py
+++ multi_llm_debate/run/big_bench/main.py
@@ -3,26 +3,26 @@ if __name__ == "__main__":
 
     from ..shared.main import main as shared_main
     from ..shared.utils import Parser
-    from .evaluate import evaluate_all_hallu_dial
-    from .run_debate import process_hallu_dial_dataset
-    from .utils import load_hallu_dial_dataset
+    from .evaluate import evaluate_all_big_bench
+    from .run_debate import process_big_bench_dataset
+    from .utils import load_big_bench_dataset
 
-    args = Parser(description="Run HalluDial evaluation").parse_args()
+    args = Parser(description="Run BIG_Bench evaluation").parse_args()
     if args.task_name is None:
-        task_name = "hallu_dial"
+        task_name = "big_bench"
     else:
         task_name = args.task_name
     print(f"Running evaluation for task: {task_name}")
     # Load the dataset
-    json_data = Path("datasets/HalluDial/")
-    dataframe = load_hallu_dial_dataset(
+    json_data = Path("datasets/BIG-Bench/")
+    dataframe = load_big_bench_dataset(
         dataset_path=json_data,
     )
 
     shared_main(
         dataframe=dataframe,
-        run_debate_fn=process_hallu_dial_dataset,
-        evaluate_fn=evaluate_all_hallu_dial,
+        run_debate_fn=process_big_bench_dataset,
+        evaluate_fn=evaluate_all_big_bench,
         task_name=task_name,
         sample_size=args.sample_size,
         config_json=args.config_json,
