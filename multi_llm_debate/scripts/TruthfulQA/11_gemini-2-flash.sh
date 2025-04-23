3fa1e86e5a60908ca3ae222e0b8d9a58a622b37a
Tyrion Huu <tyrionhuu@gmail.com>
2025-04-23 19:13:47 +0800

    fix: Correct module path for truthful_qa in multiple scripts
    Updated the module path from 'truthfual_qa' to 'truthful_qa' in
    11_gemini-2-flash.sh, 11_gemma-3-4b-it.sh, 11_llama3_1-8b.sh,
    and 11_qwen2_5-7b.sh to ensure proper execution of the evaluation.

--- multi_llm_debate/scripts/TruthfulQA/11_gemini-2-flash.sh
+++ multi_llm_debate/scripts/TruthfulQA/11_gemini-2-flash.sh
@@ -1,4 +1,4 @@
-python -m multi_llm_debate.run.truthfual_qa.main \
+python -m multi_llm_debate.run.truthful_qa.main \
     --config-json "$CONFIG" \
     --task-name "truthful_qa" \
     --sample-size 5 \
--- multi_llm_debate/scripts/TruthfulQA/11_gemma-3-4b-it.sh
+++ multi_llm_debate/scripts/TruthfulQA/11_gemma-3-4b-it.sh
@@ -105,7 +105,7 @@ CONFIG='[
 ]'
 
 # Run the evaluation using module path with direct JSON config
-python -m multi_llm_debate.run.truthfual_qa.main \
+python -m multi_llm_debate.run.truthful_qa.main \
     --task-name "truthful_qa" \
     --config-json "$CONFIG"
 
