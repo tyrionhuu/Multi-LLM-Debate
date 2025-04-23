cbc0ce989a300eaf3b11b292949903c245169e2e
Tyrion Huu <tyrionhuu@gmail.com>
2025-04-23 22:59:11 +0800

    feat: Update evaluation scripts to use judge_bench module
    Refactor evaluation commands to replace llm_bar with judge_bench
    and add task name argument for clarity in multiple scripts.

--- multi_llm_debate/scripts/JudgeBench/11_llama3_1-8b.sh
+++ multi_llm_debate/scripts/JudgeBench/11_llama3_1-8b.sh
@@ -106,7 +106,8 @@ CONFIG='[
 
 # Run the evaluation using module path with direct JSON config
 python -m multi_llm_debate.run.judge_bench.main \
-    --config-json "$CONFIG"
+    --config-json "$CONFIG" \
+    --task-name "judge_bench" 
 
 # Run the evaluation using module path with direct JSON config
 python -m multi_llm_debate.run.judge_bench.main \
