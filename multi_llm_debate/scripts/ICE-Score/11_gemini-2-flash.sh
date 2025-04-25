2069d28db079f947971087ba7249ff443f552d06
Tianyu Hu <tyrionhuu@gmail.com>
2025-04-25 16:02:25 +0800

    refactor: Update diversity pruning parameter from "embedding" to "answer"
    Removed quality pruning parameters and updated the diversity pruning
    parameter for consistency across evaluation scripts.

--- multi_llm_debate/scripts/ICE-Score/11_gemini-2-flash.sh
+++ multi_llm_debate/scripts/ICE-Score/11_gemini-2-flash.sh
@@ -23,9 +23,7 @@ CUDA_VISIBLE_DEVICES=all python -m multi_llm_debate.run.ice_score.main \
 CUDA_VISIBLE_DEVICES=all python -m multi_llm_debate.run.ice_score.main \
     --config-json "$CONFIG" \
     --task-name "ice_score_pruning" \
-    --diversity-pruning "embedding" \
+    --diversity-pruning "answer" \
     --diversity-pruning-amount 7 \
-    --quality-pruning \
-    --quality-pruning-amount 5 \
 
     
