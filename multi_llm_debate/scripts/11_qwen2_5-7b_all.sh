93049fd3b1040290a4e135955ebb91b764860bce
Tyrion Huu <tyrionhuu@gmail.com>
2025-04-28 22:53:14 +0800

    feat: Add GPU argument parsing and script execution for 11_qwen2_5-7b
    This commit introduces a new script that allows for GPU management
    by parsing the -g argument and executing multiple benchmark scripts
    with the specified GPU.

---
+++ multi_llm_debate/scripts/11_qwen2_5-7b_all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Parse -g argument for GPU number
+GPU=""
+while getopts "g:" opt; do
+    case $opt in
+        g)
+        GPU="$OPTARG"
+        ;;
+        *)
+        ;;
+    esac
+done
+
+$(pwd)/multi_llm_debate/scripts/BIG-Bench/11_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/HalluDial/11_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/ICE-Score/11_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/JudgeBench/11_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/LLMBar/11_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/TruthfulQA/11_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/COMP-Analysis/11_qwen2_5-7b.sh -g "$GPU"
