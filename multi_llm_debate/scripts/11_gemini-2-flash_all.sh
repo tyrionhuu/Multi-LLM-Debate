84760183730b010ee4efc3e662e15fe275d921e1
Tyrion Huu <tyrionhuu@gmail.com>
2025-04-29 21:50:44 +0800

    Refactor scripts and update paths for Gemini-2 and Gemma-3
    - Updated the script 11_gemini-2-flash_all.sh to replace the call to BIG-Bench with PRM800K and removed calls to JudgeBench, LLMBar, and TruthfulQA.
    - Modified 11_gemma-3-4b-it_all.sh to remove calls to ICE-Score, JudgeBench, LLMBar, and TruthfulQA, ensuring only relevant scripts are executed.
    - Changed the data path in correct_rate.ipynb from judge_bench to big_bench_pruning for consistency with the new data structure.

--- multi_llm_debate/scripts/11_gemini-2-flash_all.sh
+++ multi_llm_debate/scripts/11_gemini-2-flash_all.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-
-$(pwd)/multi_llm_debate/scripts/BIG-Bench/11_gemini-2-flash.sh
+$(pwd)/multi_llm_debate/scripts/PRM800K/11_gemini-2-flash.sh
 
 $(pwd)/multi_llm_debate/scripts/HalluDial/11_gemini-2-flash.sh
 
 $(pwd)/multi_llm_debate/scripts/ICE-Score/11_gemini-2-flash.sh
-
-$(pwd)/multi_llm_debate/scripts/JudgeBench/11_gemini-2-flash.sh
-
-$(pwd)/multi_llm_debate/scripts/LLMBar/11_gemini-2-flash.sh
-
-$(pwd)/multi_llm_debate/scripts/TruthfulQA/11_gemini-2-flash.sh
 
 $(pwd)/multi_llm_debate/scripts/COMP-Analysis/11_gemini-2-flash.sh
--- multi_llm_debate/scripts/11_gemma-3-4b-it_all.sh
+++ multi_llm_debate/scripts/11_gemma-3-4b-it_all.sh
@@ -16,14 +16,6 @@ $(pwd)/multi_llm_debate/scripts/BIG-Bench/11_gemma-3-4b-it.sh -g "$GPU"
 
 $(pwd)/multi_llm_debate/scripts/HalluDial/11_gemma-3-4b-it.sh -g "$GPU"
 
-$(pwd)/multi_llm_debate/scripts/ICE-Score/11_gemma-3-4b-it.sh -g "$GPU"
-
-$(pwd)/multi_llm_debate/scripts/JudgeBench/11_gemma-3-4b-it.sh -g "$GPU"
-
-$(pwd)/multi_llm_debate/scripts/LLMBar/11_gemma-3-4b-it.sh -g "$GPU"
-
-$(pwd)/multi_llm_debate/scripts/TruthfulQA/11_gemma-3-4b-it.sh -g "$GPU"
-
 $(pwd)/multi_llm_debate/scripts/COMP-Analysis/11_gemma-3-4b-it.sh -g "$GPU"
 
 $(pwd)/multi_llm_debate/scripts/PRM800K/11_gemma-3-4b-it.sh -g "$GPU"
