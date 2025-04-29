cee324d0b103d2aca8226a629d98d27f8271f7a3
Tyrion Huu <tyrionhuu@gmail.com>
2025-04-29 21:53:10 +0800

    refactor: Remove unused script calls and streamline execution flow
    Eliminate calls to LLMBar and TruthfulQA scripts for improved clarity.

--- multi_llm_debate/scripts/11_qwen2_5-7b_all.sh
+++ multi_llm_debate/scripts/11_qwen2_5-7b_all.sh
@@ -12,6 +12,8 @@ while getopts "g:" opt; do
     esac
 done
 
+$(pwd)/multi_llm_debate/scripts/PRM800K/11_qwen2_5-7b.sh -g "$GPU"
+
 $(pwd)/multi_llm_debate/scripts/COMP-Analysis/11_qwen2_5-7b.sh -g "$GPU"
 
 $(pwd)/multi_llm_debate/scripts/BIG-Bench/11_qwen2_5-7b.sh -g "$GPU"
@@ -22,7 +24,4 @@ $(pwd)/multi_llm_debate/scripts/ICE-Score/11_qwen2_5-7b.sh -g "$GPU"
 
 $(pwd)/multi_llm_debate/scripts/JudgeBench/11_qwen2_5-7b.sh -g "$GPU"
 
-$(pwd)/multi_llm_debate/scripts/LLMBar/11_qwen2_5-7b.sh -g "$GPU"
-
-$(pwd)/multi_llm_debate/scripts/TruthfulQA/11_qwen2_5-7b.sh -g "$GPU"
 
