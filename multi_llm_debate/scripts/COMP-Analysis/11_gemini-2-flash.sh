182fb7e3b8ff9411163b1b33e4ddb93c8f1d2dd8
Tianyu Hu <tyrionhuu@gmail.com>
2025-05-02 10:38:03 +0800

    refactor: Update task names and parameters for comp_analysis scripts
    - Change task name to "comp_analysis_pruning_all" for consistency
    - Adjust diversity pruning amount and add quality pruning options

--- multi_llm_debate/scripts/COMP-Analysis/11_gemini-2-flash.sh
+++ multi_llm_debate/scripts/COMP-Analysis/11_gemini-2-flash.sh
@@ -14,21 +14,32 @@ CONFIG='[
 ]'
 
 
-python -m multi_llm_debate.run.comp_analysis.main \
-    --config-json "$CONFIG" \
-    --task-name "comp_analysis" \
-    --batch \
-    --batch-size 11 \
-    --sample-size 1000 \
+# python -m multi_llm_debate.run.comp_analysis.main \
+#     --config-json "$CONFIG" \
+#     --task-name "comp_analysis" \
+#     --batch \
+#     --batch-size 11 \
+#     --sample-size 1000 \
 
 # Run the evaluation using module path with direct JSON config
+# python -m multi_llm_debate.run.comp_analysis.main \
+#     --config-json "$CONFIG" \
+#     --task-name "comp_analysis_pruning" \
+#     --diversity-pruning "answer" \
+#     --diversity-pruning-amount 7 \
+#     --batch \
+#     --batch-size 11 \
+#     --sample-size 1000 \
+
 python -m multi_llm_debate.run.comp_analysis.main \
     --config-json "$CONFIG" \
-    --task-name "comp_analysis_pruning" \
+    --sample-size 1000 \
+    --task-name "comp_analysis_pruning_all" \
     --diversity-pruning "answer" \
-    --diversity-pruning-amount 7 \
+    --diversity-pruning-amount 5 \
     --batch \
     --batch-size 11 \
-    --sample-size 1000 \
+    --quality-pruning \
+    --quality-pruning-amount 7 \
 
 echo "BIG-Bench Gemini-2 Flash evaluation completed. "
--- multi_llm_debate/scripts/COMP-Analysis/11_qwen2_5-7b.sh
+++ multi_llm_debate/scripts/COMP-Analysis/11_qwen2_5-7b.sh
@@ -125,7 +125,7 @@ CONFIG='[
 CUDA_VISIBLE_DEVICES=all python -m multi_llm_debate.run.comp_analysis.main \
     --config-json "$CONFIG" \
     --sample-size 1000 \
-    --task-name "comp_analysis_pruning" \
+    --task-name "comp_analysis_pruning_all" \
     --diversity-pruning "answer" \
     --diversity-pruning-amount 5 \
     --batch \
