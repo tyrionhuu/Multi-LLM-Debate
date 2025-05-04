4090f67a8f6722a1ecf0b158ca5745182d7e0ee4
Tianyu Hu <tyrionhuu@gmail.com>
2025-05-04 11:13:42 +0800

    feat: Add GPU support and command line argument parsing to multiple scripts
    - Implement GPU argument parsing in 7_qwen2_5-7b_all.sh, COMP-Analysis,
      HalluDial, ICE-Score, and JudgeBench scripts.
    - Enable tensor parallelism for improved performance across multiple GPUs.

---
+++ multi_llm_debate/scripts/7_qwen2_5-7b_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Parse -g argument for GPU number
+GPU=""
+while getopts "g:" opt; do
+    case $opt in
+        g)
+        GPU="$OPTARG"
+        ;;
+        *)
+        ;;
+    esac
+done
+
+# $(pwd)/multi_llm_debate/scripts/PRM800K/7_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/COMP-Analysis/7_qwen2_5-7b.sh -g "$GPU"
+
+# $(pwd)/multi_llm_debate/scripts/BIG-Bench/7_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/HalluDial/7_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/ICE-Score/7_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/JudgeBench/7_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/LLMBar/7_qwen2_5-7b.sh -g "$GPU"
+
+$(pwd)/multi_llm_debate/scripts/TruthfulQA/7_qwen2_5-7b.sh -g "$GPU"
