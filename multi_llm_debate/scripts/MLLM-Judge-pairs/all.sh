827b2527ae5f878078c18066c130f8c0644162c7
Tianyu Hu <tyrionhuu@gmail.com>
2025-05-06 09:29:16 +0800

    modify model length

--- multi_llm_debate/scripts/MLLM-Judge-pairs/all.sh
+++ multi_llm_debate/scripts/MLLM-Judge-pairs/all.sh
@@ -2,13 +2,18 @@
 
 # Parse -g argument for GPU number
 GPU=""
-while getopts "g:" opt; do
-    case $opt in
-        g)
-        GPU="$OPTARG"
-        ;;
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --gpu|-g)
+            GPU="$2"
+            shift 2
+            ;;
         *)
-        ;;
+            echo "Unknown option: $1"
+            echo "Usage: $0 [--gpu|-g GPU_NUMBER(S)]"
+            echo "Example: $0 --gpu 0,1 (for tensor parallelism across 2 GPUs)"
+            exit 1
+            ;;
     esac
 done
 
